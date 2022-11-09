4f5fa68c6b12f5798a9f04e6300e6b96c48d4754
Tiaan <spies.tiaan@gmail.com>
2022-11-09 11:19:26 -0500

    Added stockfish

---
+++ Stockfish/stockfish_py.py
@@ -0,0 +1,3 @@
+from stockfish import Stockfish
+
+stockfish = Stockfish(r"C:\Users\spies\OneDrive\Documents\Chess Robot\ChessRobot\Stockfish\stockfish_15_win_x64_popcnt\stockfish_15_x64_popcnt.exe")
