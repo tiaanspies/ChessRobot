2f4ecf07e7f40874dbfacb31e0b7a4eee8fbca44
Tiaan <spies.tiaan@gmail.com>
2023-04-01 21:57:39 -0400

    Completed calibration file to write final parameters to CameraProperties.out.

--- Chessboard_detection/camera_calibration.py
+++ Chessboard_detection/camera_calibration.py
@@ -1,25 +1,28 @@
 import numpy as np
 import cv2 as cv
 from pathlib import Path
+import json
 
+width = 10
+height = 7
 
 # termination criteria
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((6*7,3), np.float32)
-objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+objp = np.zeros((height*width,3), np.float32)
+objp[:,:2] = np.mgrid[0:width,0:height].T.reshape(-1,2)
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
 
-imgPath = Path("Chessboard_detection","TestImages","31_03_2023","6")
+imgPath = Path("Chessboard_detection","TestImages","01_04_2023","3")
 images = imgPath.glob("*.jpg")
-
+img = 0
 for fname in images:
     img = cv.imread(str(fname.resolve()))
-    # gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     # Find the chess board corners
-    ret, corners = cv.findChessboardCorners(gray, (7,6), None)
+    ret, corners = cv.findChessboardCorners(gray, (width,height), None)
     print(ret)
     # If found, add object points, image points (after refining them)
     if ret == True:
@@ -27,7 +30,32 @@ for fname in images:
         corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
         imgpoints.append(corners2)
         # Draw and display the corners
-        cv.drawChessboardCorners(img, (7,6), corners2, ret)
+        cv.drawChessboardCorners(img, (width,height), corners2, ret)
         cv.imshow('img', img)
         cv.waitKey(0)
-# cv.destroyAllWindows()
+cv.destroyAllWindows()
+
+ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+
+h,  w = img.shape[:2]
+newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
+
+# undistort
+dst = cv.undistort(img, mtx, dist, None, newcameramtx)
+# crop the image
+x, y, w, h = roi
+dst = dst[y:y+h, x:x+w]
+cv.imwrite('calibresult.png', dst)
+
+mean_error = 0
+for i in range(len(objpoints)):
+    imgpoints2, _ = cv.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
+    error = cv.norm(imgpoints[i], imgpoints2, cv.NORM_L2)/len(imgpoints2)
+    mean_error += error
+print( "total error: {}".format(mean_error/len(objpoints)) )
+
+# write to yaml file
+data = np.array([mtx, dist], dtype=object)
+
+with open("cameraProperties.out", "wb") as f:
+    np.save(f, data)
