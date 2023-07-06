2a4d0480ff7c4124410a028045b23e465791a572
Tiaan <spies.tiaan@gmail.com>
2023-07-05 20:09:18 -0400

    fixed test code

--- motor_commands.py
+++ motor_commands.py
@@ -1,37 +1,43 @@
 import numpy as np
-from board import SCL, SDA
-import busio
-from adafruit_pca9685 import PCA9685
-from adafruit_servokit import ServoKit
+
+try:
+    from board import SCL, SDA
+    import busio
+    from adafruit_pca9685 import PCA9685
+    from adafruit_servokit import ServoKit
+except ModuleNotFoundError:
+    print("COULD NOT FIND BOARD MODULE!!!")
 from time import sleep
 
 
 class MotorCommands:
     def __init__(self):
-        
-        # Create the I2C bus interface.
-        i2c_bus = busio.I2C(SCL, SDA)
+        try:
+            # Create the I2C bus interface.
+            i2c_bus = busio.I2C(SCL, SDA)
 
-        # Create a simple PCA9685 class instance.
-        pca = PCA9685(i2c_bus)
+            # Create a simple PCA9685 class instance.
+            pca = PCA9685(i2c_bus)
 
-        # Set the PWM frequency to 50hz.
-        pca.frequency = 50
+            # Set the PWM frequency to 50hz.
+            pca.frequency = 50
 
-        # Create a servokit class instance with the total number of channels needed
-        kit = ServoKit(channels=16)
+            # Create a servokit class instance with the total number of channels needed
+            kit = ServoKit(channels=16)
 
-        # define shortcuts and set pin numbers for each motor
-        self.base = kit.servo[1]
-        self.shoulder = kit.servo[2]
-        self.elbow = kit.servo[3]
-        self.grip = kit.servo[4]
+            # define shortcuts and set pin numbers for each motor
+            self.base = kit.servo[1]
+            self.shoulder = kit.servo[2]
+            self.elbow = kit.servo[3]
+            self.grip = kit.servo[4]
 
-        # set all pulsewidth ranges
-        self.base.set_pulse_width_range(500,2500)
-        self.shoulder.set_pulse_width_range(500,2500)
-        self.elbow.set_pulse_width_range(500,2500)
-        self.grip.set_pulse_width_range(500,2500) # TODO: this one is probably different
+            # set all pulsewidth ranges
+            self.base.set_pulse_width_range(500,2500)
+            self.shoulder.set_pulse_width_range(500,2500)
+            self.elbow.set_pulse_width_range(500,2500)
+            self.grip.set_pulse_width_range(500,2500) # TODO: this one is probably different
+        except NameError:
+            print("COULD NOT LOAD")
 
         self.OPEN = np.pi/4 # TODO: replace this with the angle needed for it to be open (in radians)
         self.CLOSED = 3*np.pi/4 # TODO: replace this with the angle needed for it to be closed (in radians)
--- test2.py
+++ test2.py
@@ -17,37 +17,52 @@ def draw_cube(v, slice_num):
     step = 10
     path = cm.quintic_line(cm.HOME, top_left, step)
     
-    slice_width = v["far"] - v["close"] / slice_num
+    slice_width = (v["far"] - v["close"]) / slice_num
+    slice_step = np.array([0.0, slice_width, 0.0], dtype=int)
+
     for _ in range(slice_num):
         path = np.hstack((path, \
                           cm.quintic_line(top_left, top_right, step), \
                           cm.quintic_line(top_right, bottom_left, step), \
                           cm.quintic_line(bottom_left, bottom_right, step), \
                           cm.quintic_line(bottom_right, top_left, step), \
-                          cm.quintic_line(top_left, top_left + np.array([0, slice_width, 0]), step)))
+                          cm.quintic_line(top_left, top_left + slice_step, step)))
+        
+        top_left += slice_step
+        top_right += slice_step
+        bottom_left += slice_step
+        bottom_right += slice_step
+
+    path = np.hstack((path, cm.quintic_line(top_left, cm.HOME, step)))  
+
+    return path
         
 def main():
 
     vertices = {
-        "top" : 320,
-        "bottom" : 20,
-        "right" : 160,
-        "left" : -160,
-        "close" : 20,
-        "far" : 320}
+        "top" : 260,
+        "bottom" : 100,
+        "right" : 80,
+        "left" : -80,
+        "close" : 100,
+        "far" : 260}
 
-    path = cm.draw_cube(vertices, 4) # generate waypoints
+    path = draw_cube(vertices, 4) # generate waypoints
     print("path generated")
-    plt.plot(path[0,:], path[1,:], path[2,:])
+    
+    ax = plt.axes(projection='3d')
+    ax.scatter(path[0,:], path[1,:], path[2,:])
+    plt.show()
 
     print("solving inverse kinematics...")
     thetas = cm.inverse_kinematics(path) # convert to joint angles
     grip_commands = cm.get_gripper_commands2(path) # remove unnecessary wrist commands, add gripper open close instead
     plan = mc.sort_commands(thetas, grip_commands)
     print("solved!")
+    cm.plot_robot(thetas, path)
 
     np.save("plan.npy",plan)
-    # plan = np.load("plan.npy",)
+    plan = np.load("plan.npy",)
     
     # mc.run(plan)
 
