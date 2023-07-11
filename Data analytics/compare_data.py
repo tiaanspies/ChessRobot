679bc57b1079c2db51795f73b71c05779504a69d
Tiaan <spies.tiaan@gmail.com>
2023-07-11 16:42:17 -0400

    Data analytics

---
+++ Data analytics/compare_data.py
@@ -0,0 +1,37 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+# Load the numpy file
+# data = np.load('positions 4.npy')
+# data_target = np.load('path_bigXs.npy').T
+
+data = np.load('positions 5.npy')[165:, :]
+data_target = np.load('path_board.npy').T
+
+data_target[:, [0, 1, 2]] = data_target[:, [1, 2, 0]]
+data_target = data_target[data_target[:, 1] == 100]
+
+# Create a figure and a grid of subplots
+fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(12, 4))
+
+for axis in axes:
+    axis.axis('equal')
+    # axis.set_aspect('equal', 'box')
+
+pass
+
+def plot_3data(axes, data):
+    # Plot data on each subplot
+    axes[0].plot(data[:, 0], data[:, 1])
+    axes[0].set_title('Plot 1')
+
+    axes[1].plot(data[:, 1], data[:, 2])
+    axes[1].set_title('Plot 2')
+
+    axes[2].plot(data[:, 0], data[:, 2])
+    axes[2].set_title('Plot 3')
+
+plot_3data(axes, data)
+plot_3data(axes, data_target)
+plt.show()
--- Data analytics/plot.py
+++ Data analytics/plot.py
@@ -3,26 +3,34 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 # Load the numpy file
-data = np.load('positions 5.npy')
+data = np.load('positions 4.npy')
+data_target = np.load('path_bigXs.npy').T
 
-# Extract x, y, and z coordinates
-x = data[:, 0]
-y = data[:, 2]
-z = data[:, 1]
+data_target[:, [0, 1, 2]] = data_target[:, [1, 2, 0]]
+
+def plot_3data(data, ax):
+    # Extract x, y, and z coordinates
+    x = data[:, 0]
+    y = data[:, 2]
+    z = data[:, 1]
+
+    ax.scatter(x, y, z)
+
+    # Set labels and title
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    ax.set_title('3D Scatter Plot')
+    ax.set_xlim([0, 500])  # Replace xmin and xmax with your desired limits
+    ax.set_ylim([-400, 400])  # Replace ymin and ymax with your desired limits
+    ax.set_zlim([0, 600])
 
 # Create a 3D scatter plot
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-ax.scatter(x, y, z)
 
-# Set labels and title
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Z')
-ax.set_title('3D Scatter Plot')
-ax.set_xlim([0, 500])  # Replace xmin and xmax with your desired limits
-ax.set_ylim([-400, 400])  # Replace ymin and ymax with your desired limits
-ax.set_zlim([0, 600])
+plot_3data(data, ax)
+plot_3data(data_target, ax)
 
 # Show the plot
 plt.show()
