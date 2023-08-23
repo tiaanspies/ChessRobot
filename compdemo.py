63b1df3592aa8e52b3f820ebe852a967f2f9e38e
Knowajones <noahmjones22@gmail.com>
2023-08-22 22:14:53 -0400

    added Tiaan's stuff

--- compdemo.py
+++ compdemo.py
@@ -1,9 +1,15 @@
 from IK_Solvers.traditional import ChessMoves
 from motor_commands import MotorCommands
+from Data_analytics import correction_transform
 import numpy as np
 
 cm = ChessMoves(lift=200)
 mc = MotorCommands()
+
+# ==================Using transformation matrix============
+# load transformation matrix
+print("finding transform")
+H, T, real_mean = correction_transform.get_transform("positions_day2.npy", "path_big_day2.npy")
 
 def generate_path():
     pickup_square = input("Pick up square:")
@@ -12,6 +18,14 @@ def generate_path():
     goal = cm.get_coords(drop_square)
 
     path = cm.generate_quintic_path(start, goal, None) # generate waypoints
+    
+    print("Updating points")
+    # change between coordinate systems
+    path_optitrack_sys = correction_transform.to_optitrack_sys(path)
+    projected_points = correction_transform.project_points(path_optitrack_sys, real_mean, T, H)
+    projected_points = correction_transform.from_optitrack_sys(projected_points)
+
+    print("Inverse Kinematics")
     thetas = cm.inverse_kinematics(path) # convert to joint angles
     grip_commands = cm.get_gripper_commands2(path) # remove unnecessary wrist commands, add gripper open close instead
     return mc.sort_commands(thetas, grip_commands)
@@ -34,6 +48,7 @@ def main():
             path = rev_path
 
         forward = not forward
+        input("ready?")
         mc.run(path)
     
 if __name__ == "__main__":
