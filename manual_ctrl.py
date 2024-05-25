7ddcfc59f4ebc8a3be403eb3a1bafbbe1d3178e1
Tiaan <spies.tiaan@gmail.com>
2024-05-25 19:39:07 -0400

    Manual moves

--- manual_ctrl.py
+++ manual_ctrl.py
@@ -1,6 +1,19 @@
 import chess_manager
+import logging
 
 def manual_control_menu():
+    print('Please select an option:')
+    print('1. Point-to-point move')
+    print('2. Move to home position')
+
+    choice = input('Enter your choice: ')
+
+    if choice == '1':
+        pt2pt_move()
+    elif choice == '2':
+        move_to_home()
+
+def pt2pt_move():
     print("Enter start and end positions of the piece you want to move.")
 
     start_pos = input('Enter start position: ')
@@ -8,6 +21,12 @@ def manual_control_menu():
 
     chess_manager.robotsPhysicalMove(start_pos+end_pos, None)
 
+def move_to_home():
+    """Move to the home position"""
+    ja = chess_manager.motion_planner.inverse_kinematics(chess_manager.motion_planner.HOME)
+    chess_manager.motor_driver.run(ja)
+
+    logging.info("Moved to home position")
 
 if __name__ == '__main__':
     
