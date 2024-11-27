8357bb9669336c7ddab88f07755cb48cb596d999
Tiaan Spies <spies.tiaan@gmail.com>
2024-11-26 21:33:48 -0500

    Basic commands working

--- Positioning/hiwonder_control.py
+++ Positioning/hiwonder_control.py
@@ -1,37 +1,38 @@
 import serial
 import time
 
-def calculate_checksum(data):
-    checksum = sum(data) & 0xFF
-    return ~checksum & 0xFF
-
-def create_command_packet(servo_id, command, parameters):
-    length = 3 + len(parameters)
-    packet = [0x55, 0x55, servo_id, length, command] + parameters
-    checksum = calculate_checksum(packet[2:])
-    packet.append(checksum)
+def create_command_packet(command, parameters):
+    length = 2 + len(parameters)
+    packet = [0x55, 0x55, length, command] + parameters
     return bytearray(packet)
 
-def send_command(ser, servo_id, command, parameters):
-    packet = create_command_packet(servo_id, command, parameters)
+def send_command(ser, command, parameters):
+    packet = create_command_packet(command, parameters)
     ser.write(packet)
     time.sleep(0.1)  # Wait for the servo to process the command
-    response = ser.read_all()
-    return response
+
+def parameters_for_position(time, motor_ids, positions):
+    
+    servo_num = len(motor_ids) # Parameter 1
+    time_hex_low = time & 0xFF # Parameter 2 (Low 8 bits of time)
+    time_hex_high = (time >> 8) & 0xFF # parameter 3 (High 8 bits of time)
+
+    position_parameters = [[id, position & 0xFF, (position >> 8) & 0xFF] for id, position in zip(motor_ids, positions)]
+    flat_position = [x for xs in position_parameters for x in xs] # Flatten the list
+    
+    parameters = [servo_num, time_hex_low, time_hex_high] + flat_position
+
+    return parameters
+
+time = 1000
+motor_ids = [1, 2, 3]
+positions = [1000, 500, 750]
+position_params = parameters_for_position(time, motor_ids, positions)
 
 # Open the serial port
-ser = serial.Serial('/dev/ttyS0', 115200, timeout=1)
+ser = serial.Serial('/dev/ttyS0', 9600, timeout=1)
 
-# Example: Send a command to servo with ID 1 to move to position 512
-servo_id = 1
-command = 0x01  # Example command for position control
-parameters = [0x02, 0x00]  # Example parameters for position 512 (0x0200)
-response = send_command(ser, servo_id, command, parameters)
-
-print("Response:", response)
+send_command(ser, 3, position_params)  # Send the command
 
 # Close the serial port
 ser.close()
-def receive_data(ser):
-    data = ser.read_all()
-    return data
