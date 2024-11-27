e1e28e0899e25423c9ea976dd13610aec757c39f
Tiaan Spies <spies.tiaan@gmail.com>
2024-11-26 22:08:31 -0500

    Basic working function

--- Positioning/hiwonder_control.py
+++ Positioning/hiwonder_control.py
@@ -11,11 +11,14 @@ def send_command(ser, command, parameters):
     ser.write(packet)
     time.sleep(0.1)  # Wait for the servo to process the command
 
-def parameters_for_position(time, motor_ids, positions):
+def parameters_for_position(move_time, motor_ids, positions):
     
+    # make sure min move time is 200ms
+    move_time = max(200, move_time)
+
     servo_num = len(motor_ids) # Parameter 1
-    time_hex_low = time & 0xFF # Parameter 2 (Low 8 bits of time)
-    time_hex_high = (time >> 8) & 0xFF # parameter 3 (High 8 bits of time)
+    time_hex_low = move_time & 0xFF # Parameter 2 (Low 8 bits of time)
+    time_hex_high = (move_time >> 8) & 0xFF # parameter 3 (High 8 bits of time)
 
     position_parameters = [[id, position & 0xFF, (position >> 8) & 0xFF] for id, position in zip(motor_ids, positions)]
     flat_position = [x for xs in position_parameters for x in xs] # Flatten the list
@@ -24,10 +27,11 @@ def parameters_for_position(time, motor_ids, positions):
 
     return parameters
 
-time = 1000
-motor_ids = [1, 2, 3]
-positions = [1000, 500, 750]
-position_params = parameters_for_position(time, motor_ids, positions)
+move_time = 3000
+motor_ids = [5, 6, 7]
+positions = [600, 1000, 500]
+positions = [400, 1000, 1000]
+position_params = parameters_for_position(move_time, motor_ids, positions)
 
 # Open the serial port
 ser = serial.Serial('/dev/ttyS0', 9600, timeout=1)
