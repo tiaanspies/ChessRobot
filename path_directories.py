a8406ed21e380db2106e3bc7ac1d0bbff609f2ce
Tiaan <spies.tiaan@gmail.com>
2024-12-14 22:48:55 -0500

    Update google drive paths

--- path_directories.py
+++ path_directories.py
@@ -6,9 +6,9 @@ PLANNED_PATHS = Path("/home", "tpie", "mnt", "gdrive", "Planned Paths")
 H_MATRIX_PATH = Path("/home", "tpie", "mnt", "gdrive", "Transformation Matrices")
 RUN_PATH = Path("/home", "tpie", "mnt", "gdrive", "Run Path")
 
-PATH_WIN_CAL_TRACKING_DATA = Path('G:\My Drive\Programming\RPiCLone\Arm Cal Tracking Data')
-PATH_WIN_PLANNED_PATHS = Path('G:\My Drive\Programming\RPiCLone\Planned Paths')
-PATH_WIN_H_MATRIX_PATH = Path('G:\My Drive\Programming\RPiCLone',"Transformation Matrices")
-PATH_WIN_RUN_PATH = Path('G:\My Drive\Programming\RPiCLone\Ru n Path')
+PATH_WIN_CAL_TRACKING_DATA = Path('G:\My Drive\Projects\Programming\RPiCLone\Arm Cal Tracking Data')
+PATH_WIN_PLANNED_PATHS = Path('G:\My Drive\Projects\Programming\RPiCLone\Planned Paths')
+PATH_WIN_H_MATRIX_PATH = Path('G:\My Drive\Projects\Programming\RPiCLone',"Transformation Matrices")
+PATH_WIN_RUN_PATH = Path('G:\My Drive\Projects\Programming\RPiCLone\Ru n Path')
 
 CONFIG_PATH_KINEMATICS = Path("config", "kinematics.yml")
