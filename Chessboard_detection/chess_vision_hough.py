f1ff4eb682be5b1634273b199cc29593747b2f15
Tiaan <spies.tiaan@gmail.com>
2025-02-11 21:00:10 -0500

    Can differentiate between empty square and square with piece based on the standard deviation of the HSV color space.
    Investigating methods of determining which color piece is on the square.

---
+++ Chessboard_detection/chess_vision_hough.py
@@ -0,0 +1,354 @@
+import cv2
+import numpy as np
+from pathlib import Path
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
+from sklearn.cluster import MeanShift, estimate_bandwidth
+
+import Chessboard_detection.board_detection_hough as bdh
+import Chessboard_detection.projection as proj
+
+
+
+class ChessVisionHough:
+    def __init__(self, img):
+        # Process image into individual squares snippets (Array of 64 squares each 2D numpy array with 3 BGR channels)
+        squares = proj.process_image(img)
+
+        # preprocess the squares (apply Gaussian blur and convert to HSV)
+        blurred_hsv_squares = [preprocess_square(square) for square in squares]
+
+        # get the linear regression model to determine if a square has a piece or not.
+        self.model_piece_vs_empty = self.get_piece_vs_empty_model(blurred_hsv_squares)
+
+        # w_o_w, w_o_b, b_o_w, b_o_b, e_w, e_b = get_init_board_groups()
+
+    def get_piece_vs_empty_model(self, squares):
+        """
+        Inputs a list of 64 squares and returns a logistic regression model that determines if a square has a piece or not.
+        """
+        features = extract_features(squares)
+        labels = get_init_board_labels_piece_v_empty()
+        model = get_logistic_regression_model(features, labels)
+
+        # plot_decision_boundary(model, features, labels)
+
+        return model
+
+    def indentify_piece_ids(self, img):
+        """
+        Directly find chessboard corners in img2 and warps it to square and crops it.
+        """
+        squares = proj.process_image(img)
+
+        # preprocess the squares (apply Gaussian blur and convert to HSV)
+        blurred_hsv_squares = [preprocess_square(square) for square in squares]
+
+        piece_v_empty = self.model_piece_vs_empty.predict(extract_features(blurred_hsv_squares))
+
+        for i in range(64):
+            if piece_v_empty[i] == 1:
+                piece_v_empty[i] = determine_piece_color(squares[i])
+        return [-1] * 64
+    
+    def label_chessboard(self, img):
+        """
+        Labels the chessboard squares with detected pieces and their background colors.
+        This function takes an image of a chessboard, detects the pieces on each square,
+        and assigns a label to each square indicating the piece and the background color.
+        Args:
+            img (numpy.ndarray): The input image of the chessboard.
+        Returns:
+            dict: A dictionary where each key is a square identifier (e.g., "A1", "B1", etc.)
+                and the value is another dictionary with keys "piece" and "background".
+                The "piece" key maps to the detected piece on that square (as a string),
+                and the "background" key maps to the background color of the square.
+        Example:
+            >>> img = cv2.imread('chessboard.jpg')
+            >>> labels = label_chessboard(img)
+            >>> print(labels["A1"])
+            {'piece': 'white', 'background': 'white'}
+        """
+        label_ids = self.indentify_piece_ids(img)
+        labels_words = convert_ids_to_words(label_ids)
+
+        square_colour = board_background_labels()
+
+        SQUARES = [
+            "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1",
+            "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2",
+            "a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3",
+            "a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4",
+            "a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5",
+            "a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6",
+            "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7",
+            "a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8",
+        ]
+
+        label_dict = {}
+
+        for i in range(64):
+            label_dict[SQUARES[i]] = {"piece": labels_words[i], "background": square_colour[i]}
+
+        return label_dict
+    
+def determine_piece_color(square):
+    """
+    Use canny edge detection to find the outline of the piece in the square.
+    Then use the color of the pixels inside the outline to determine the color of the piece.
+    """
+    # # Convert the image to the LUV color space
+    # luv_square = cv2.cvtColor(square, cv2.COLOR_BGR2LUV)
+
+    # # Apply mean shift segmentation
+    # shifted = cv2.pyrMeanShiftFiltering(luv_square, sp=11, sr=11)
+
+    # # Convert back to BGR color space
+    # output = cv2.cvtColor(shifted, cv2.COLOR_LUV2BGR)
+    # proj.showImg(output)
+
+    # filter to reduce noise
+    img = cv2.medianBlur(square, 3)
+
+    # flatten the image
+    flat_image = img.reshape((-1,3))
+    flat_image = np.float32(flat_image)
+
+    # meanshift
+    bandwidth = estimate_bandwidth(flat_image, quantile=.2)
+    ms = MeanShift(bandwidth=bandwidth, max_iter=800, bin_seeding=True)
+    ms.fit(flat_image)
+    labeled=ms.labels_
+
+
+    # get number of segments
+    segments = np.unique(labeled)
+    print('Number of segments: ', segments.shape[0])
+
+    # get the average color of each segment
+    total = np.zeros((segments.shape[0], 3), dtype=float)
+    count = np.zeros(total.shape, dtype=float)
+    for i, label in enumerate(labeled):
+        total[label] = total[label] + flat_image[i]
+        count[label] += 1
+    avg = total/count
+    avg = np.uint8(avg)
+
+    # cast the labeled image into the corresponding average color
+    res = avg[labeled]
+    result = res.reshape((img.shape))
+
+    proj.showImg(result)
+
+    return 1
+
+def extract_features(squares):
+    features = []
+    for square in squares:
+        mean_s, std_s = cv2.meanStdDev(square[:,:,1])
+        mean_v, std_v = cv2.meanStdDev(square[:,:,2])
+
+        features.append([std_s[0,0], std_v[0,0]])
+
+    return np.array(features)
+
+def get_logistic_regression_model(features, labels):
+    """
+    Inputs a preprocessed HSV image of the sqyare.
+
+    Fits a Gaussian curve to the histogram of the S and V channels to determine if there is a empty square or not.
+
+    Empty squares will have a single prominent peak in the S and V channels where with a piece it will be bimodal or more spread out.
+    """
+
+    model = LogisticRegression()
+    model.fit(features, labels)
+
+    return model
+
+def get_init_board_labels_piece_v_empty():
+    """
+    
+    Return an array [64] where 1 is a piece and 0 is empty.
+    """
+
+    labels = [1]*16+[0]*32+[1]*16
+
+    return labels
+
+def preprocess_square(square):
+    blurred = cv2.GaussianBlur(square, (5, 5), 0)
+    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+    return hsv
+
+def plot_decision_boundary(model, features, labels):
+    """
+    Plots the decision boundary of the logistic regression model along with the data points.
+    """
+    x_min, x_max = features[:, 0].min() - 1, features[:, 0].max() + 1
+    y_min, y_max = features[:, 1].min() - 1, features[:, 1].max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.1), np.arange(y_min, y_max, 0.1))
+    Z = model.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+
+    plt.figure(figsize=(10, 6))
+    plt.contourf(xx, yy, Z, alpha=0.4)
+    plt.scatter(features[:, 0], features[:, 1], c=labels, edgecolors='k', marker='o')
+    plt.xlabel('Standard Deviation of S Channel')
+    plt.ylabel('Standard Deviation of V Channel')
+    plt.title('Logistic Regression Decision Boundary')
+    plt.show()
+
+def convert_ids_to_words(labels):
+    """
+    Coverts IDS 1, 0, -1 to correspnding colors white, none and black.
+
+    Parameters:
+    labels: array[64] of ids.
+    Returns:
+    array[64] of strings.
+    """
+
+    mapping = ["black", "none", "white"]
+    labels = [mapping[id+1] for id in labels]
+    
+    return labels
+
+def board_background_labels():
+    """
+    Returns the labels for the background of the chessboard.
+
+    returns:
+    array[64] of strings.
+    """
+
+    labels = []
+    for i in range(8):
+        for j in range(8):
+            if (i+j) % 2 == 0:
+                labels.append("black")
+            else:
+                labels.append("white")
+                
+    return labels
+    
+
+def label_dict_to_str(label_dict):
+    """
+    Inputs a dict label_dict[SQUARES[i]] = {"piece": labels_words[i], "background": square_colour[i]}
+
+    outputs a dict label_dict_words[SQUARES[i]] = f"{labels_words[i]} on {square_colour[i]}"
+    """
+
+    label_dict_words = {}
+
+    for square, labels in label_dict.items():
+        label_dict_words[square] = f"{labels['piece']} on {labels['background']}"
+
+    return label_dict_words
+
+
+def get_init_board_groups():
+    """
+    Returns the IDS for the initial board configuration. The 6 groups are white pieces on white squares, white pieces on black squares, 
+    black pieces on white squares, black pieces on black squares, empty white squares and empty black squares.
+
+    returns:
+    white_on_white: array[8] of ids.
+    white_on_black: array[8] of ids.
+    black_on_white: array[8] of ids.
+    black_on_black: array[8] of ids.
+    empty_white: array[16] of ids.
+    empty_black: array[16] of ids.
+    """
+
+    white_on_white = [1,3,5,7,8,10,12,14]
+    white_on_black = [0,2,4,6,9,11,13,15]
+    empty_white = [17,19,21,23,24,26,28,30,33,35,37,39,40,42,44,46]
+    black_on_white = [48,50,52,54,57,59,61,63]
+    black_on_black = [49,51,53,55,56,58,60,62]
+    empty_black = [16,18,20,22,25,27,29,31,32,34,36,38,41,43,45,47]
+
+    return white_on_white, white_on_black, black_on_white, black_on_black, empty_white, empty_black
+
+def display_squares(squares, contrast_squares, preprocessed_squares, has_pieces):
+    """
+    Display 3 rows of 8 squares each. Each square is displayed with the original, preprocessed and has_piece image.
+        
+    """
+    for i in range(8):
+        plt.figure(figsize=(17, 9))
+        for j in range(8):
+            plt.subplot(4, 8, 1 + j)
+            plt.title('Original Image')
+            plt.imshow(cv2.cvtColor(squares[i*8+j],cv2.COLOR_BGR2RGB))
+
+            # plt.subplot(4, 8, 9 + j)
+            # plt.title('Enhanced Contrast')
+            # plt.imshow(contrast_squares[i*8+j], cmap='gray')
+
+            # mean_s, std_s = cv2.meanStdDev(preprocessed_squares[i*8+j][:,:,1])
+            # mean_v, std_v = cv2.meanStdDev(preprocessed_squares[i*8+j][:,:,2])            
+
+            # plt.subplot(4, 8, 17 + j)
+            # plt.title('Histogram (S) P:'+str(has_pieces[i*8+j]))
+            # plt.hist(preprocessed_squares[i*8+j][:,:,1].ravel(), bins=256, range=[0, 256], color='black')
+
+            # plt.subplot(4, 8, 25 + j)
+            # plt.title(str((std_s)) + ' ' + str(std_v))
+            # plt.hist(preprocessed_squares[i*8+j][:,:,2].ravel(), bins=256, range=[0, 256], color='black')
+    
+        plt.show()
+
+
+def display_group(group1, group2, group3, group4, group_1_name, group_2_name, group_3_name, group_4_name):
+    """
+    Display 4 rows of 8 squares each. Each square is displayed with the original, preprocessed and has_piece image.
+        
+    """
+    for i in range(len(group1)//8):
+        plt.figure(figsize=(17, 9))
+        for j in range(8):
+
+            hsv_img1 = cv2.cvtColor(group1[i*8+j], cv2.COLOR_BGR2HSV)
+            hsv_img2 = cv2.cvtColor(group2[i*8+j], cv2.COLOR_BGR2HSV)
+            hsv_img3 = cv2.cvtColor(group3[i*8+j], cv2.COLOR_BGR2HSV)
+            hsv_img4 = cv2.cvtColor(group4[i*8+j], cv2.COLOR_BGR2HSV)
+
+            plt.subplot(4, 8, j + 1)
+            plt.title('H Channel')
+            plt.hist(hsv_img1[:, :, 0].ravel(), bins=180, range=[0, 180], color='blue', alpha=0.5, label=group_1_name)
+            plt.hist(hsv_img2[:, :, 0].ravel(), bins=180, range=[0, 180], color='red', alpha=0.5, label=group_2_name)
+            plt.hist(hsv_img3[:, :, 0].ravel(), bins=180, range=[0, 180], color='green', alpha=0.5, label=group_3_name)
+            plt.hist(hsv_img4[:, :, 0].ravel(), bins=180, range=[0, 180], color='orange', alpha=0.5, label=group_4_name)
+            plt.legend()
+
+            plt.subplot(4, 8, 9 + j)
+            plt.title('S Channel')
+            plt.hist(hsv_img1[:, :, 1].ravel(), bins=256, range=[0, 256], color='blue', alpha=0.5, label=group_1_name)
+            plt.hist(hsv_img2[:, :, 1].ravel(), bins=256, range=[0, 256], color='red', alpha=0.5, label=group_2_name)
+            plt.hist(hsv_img3[:, :, 1].ravel(), bins=256, range=[0, 256], color='green', alpha=0.5, label=group_3_name)
+            plt.hist(hsv_img4[:, :, 1].ravel(), bins=256, range=[0, 256], color='orange', alpha=0.5, label=group_4_name)
+            plt.legend()
+
+            plt.subplot(4, 8, 17 + j)
+            plt.title('V Channel')
+            plt.hist(hsv_img1[:, :, 2].ravel(), bins=256, range=[0, 256], color='blue', alpha=0.5, label=group_1_name)
+            plt.hist(hsv_img2[:, :, 2].ravel(), bins=256, range=[0, 256], color='red', alpha=0.5, label=group_2_name)
+            plt.hist(hsv_img3[:, :, 2].ravel(), bins=256, range=[0, 256], color='green', alpha=0.5, label=group_3_name)
+            plt.hist(hsv_img4[:, :, 2].ravel(), bins=256, range=[0, 256], color='orange', alpha=0.5, label=group_4_name)
+            plt.legend()
+    
+        plt.show()
+
+    
+def main():
+    img_path2 = str(Path("Chessboard_detection", "TestImages", "Temp", "a2.jpg"))
+    img_starting_pos = cv2.imread(img_path2)
+
+    chess_vision = ChessVisionHough(img_starting_pos)
+
+    chess_vision.label_chessboard(img_starting_pos)
+
+if __name__ == "__main__":
+    main()
--- Chessboard_detection/projection.py
+++ Chessboard_detection/projection.py
@@ -1,12 +1,10 @@
-import random
 import cv2 as cv
 from pathlib import Path
 from time import sleep
 from Chessboard_detection import board_detection_hough as bdh
 import numpy as np
-import heapq
 import matplotlib.pyplot as plt
-import chess
+from sklearn.linear_model import LogisticRegression
 
 """
 Manages image projection and perspective transformations so that the chessboard pattern standardized in the image.
@@ -24,11 +22,12 @@ def project_board_2_square(img, corners_src):
 
     returns an image that is filled with the chessboard pattern and is square in the image.
     """
+    IMG_SIZE = 304
     corners_src_outer = np.array([corners_src[0, 0], corners_src[0, -1], corners_src[-1, 0], corners_src[-1, -1]], dtype=np.float32)
-    corners_dest = np.array([[0,0], [300, 0], [0, 300], [300, 300]], dtype=np.float32)
+    corners_dest = np.array([[0,0], [IMG_SIZE, 0], [0, IMG_SIZE], [IMG_SIZE, IMG_SIZE]], dtype=np.float32)
 
     M = cv.getPerspectiveTransform(corners_src_outer, corners_dest)
-    img_warped = cv.warpPerspective(img, M, (300, 300))
+    img_warped = cv.warpPerspective(img, M, (IMG_SIZE, IMG_SIZE))
 
     return img_warped
 
@@ -67,31 +66,6 @@ def showImg(img):
     while cv.waitKey(1) != ord('q'):
         sleep(0.1)
 
-def preprocess_square(square):
-    blurred = cv2.GaussianBlur(square, (5, 5), 0)
-    _, thresh = cv2.threshold(blurred, 0, 255, cv2.THRESH_OTSU)
-    return thresh
-
-def has_piece(thresh, min_contour_area=100):
-    """
-    Detects contours in a binary image (thresh) and returns True if the contour area is greater than min_contour_area.
-
-    Parameters:
-    thresh: Binary image.
-    min_contour_area: Minimum contour area to be considered as a piece.
-    returns:
-    True if the contour area is greater than min_contour_area
-    """
-    contours, _ = cv.findContours(thresh, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-    for cnt in contours:
-        area = cv.contourArea(cnt)
-    
-    color_thresh = cv.cvtColor(thresh, cv.COLOR_GRAY2RGB)
-    cv.drawContours(color_thresh, contours, -1, (0, 0, 255), 2)
-
-    
-    return len(contours) > 0, color_thresh
-
 def crop_squared_by_border_width(squares, border_width=2):
     """
     Crops the square by the border width to remove the border of the square.
@@ -108,151 +82,20 @@ def crop_squared_by_border_width(squares, border_width=2):
         cropped_squares.append(cropped_square)
     return cropped_squares
 
-def crop_outer_corners(img, corners):
+def process_image(img):
     """
-    Inputs a topdown image of the chessboard. Crops using previously found corners to only contain the board.
+    Pipeline to find chessboard, split into squares and crop away the border.
 
-    Returns a cropped image of the chessboard pattern.
-    """
-    corners_src_outer = np.array([corners[0, 0], corners[0, -1], corners[-1, 0], corners[-1, -1]])
-    
-    minX = np.min(corners_src_outer[:, 0])
-    minY = np.min(corners_src_outer[:, 1])
-    
-    maxX = np.max(corners_src_outer[:, 0])
-    maxY = np.max(corners_src_outer[:, 1])
-
-    pattern = img[minY:maxY, minX:maxX] 
-
-    return pattern
-
-def find_template_location(img1, template):
-    """
-    Scan template across img1 to find the matching location for the pattern.
-    
     Parameters:
-    img1: Full size image.
-    template: Smaller pattern image.
-    
+    img: Image of the chessboard pattern.
+
     Returns:
-    Top-left corner of the matching location.
-    """
-    w, h = template.shape[:2]
-
-    result = cv.matchTemplate(img1, template, cv.TM_CCOEFF_NORMED)
-    min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
- 
-    return max_loc
-
-def chessboard_detection_1_template_matching(img1, img2):
-    """
-    Uses template matching to to match the location of the chessboard in img2 to img1. Then warps and crops img2 using corners from img1.
-
-    Parameters:
-    img1: Full size image.
-    img2: Full size image.
-
-    """
-    all_corners = bdh.find_board_corners(img1)
-
-    # find offsets to overlay img 1 and 2 perfectly
-    cropped_image = crop_outer_corners(img2, all_corners)    
-    showImg(cropped_image)
-    top_left = find_template_location(img1, cropped_image)
-
-    # Replace the location at max_loc in img1 with the cropped image pattern
-    bottom_right = (top_left[0] + cropped_image.shape[1], top_left[1] + cropped_image.shape[0])
-
-    # Ensure the replacement area is within the bounds of img1
-    if bottom_right[0] <= img1.shape[1] and bottom_right[1] <= img1.shape[0]:
-        img1[top_left[1]:bottom_right[1], top_left[0]:bottom_right[0]] = cropped_image
-        showImg(img1)
-    else:
-        print("Replacement area is out of bounds.")
-
-    img_warped = project_board_2_square(img1, all_corners)
-    showImg(img_warped)
-
-def chessboard_detection_2_orb_transform(img1, img2):
-    """
-    Find the homographic transform to match img2 onto img1 using sift transform or similar.
-
-    Then project the chessboard pattern in img2 to be square in the image using detected corners from img1.
+    List of 64 square images of the chessboard pattern.
     """
 
-    # find all the corner location in img1
-    all_corners = bdh.find_board_corners(img1)
-
-    # Find and match featuers between img1 and img2
-    descriptor = cv.ORB_create()
-    bf = cv.BFMatcher(cv.NORM_HAMMING, crossCheck=True)
-
-    kp1, des1 = descriptor.detectAndCompute(img1,None)
-    kp2, des2 = descriptor.detectAndCompute(img2,None)
-    
-    # Match descriptors.
-    matches = bf.match(des1,des2)
-    valid_matches = np.array(heapq.nsmallest(100, matches, key = lambda x: x.distance))
-
-    # Get the co-ordinated of the selected matches
-    point1_coords = np.zeros((len(valid_matches), 2), dtype=np.float32)
-    point2_coords = np.zeros((len(valid_matches), 2), dtype=np.float32)
-    for i, match in enumerate(valid_matches):
-        point1_coords[i] = kp1[match.queryIdx].pt
-        point2_coords[i] = kp2[match.trainIdx].pt
-
-    # Find the best homography using a modified RANSAC algorithm
-    M, mask = cv.findHomography(point2_coords, point1_coords, cv.USAC_ACCURATE, confidence=1)
-
-    # Warp img2 to img1
-    img2_warped_to_img1 = cv.warpPerspective(img2, M, (img1.shape[1], img1.shape[0]))
-
-    img2_warped_to_img1 = project_board_2_square(img2_warped_to_img1, all_corners)
-
-def display_squares(squares, contrast_squares, preprocessed_squares, has_pieces):
-    """
-    Display 3 rows of 8 squares each. Each square is displayed with the original, preprocessed and has_piece image.
-        
-    """
-    for i in range(8):
-        plt.figure(figsize=(17, 9))
-        for j in range(8):
-            plt.subplot(4, 8, 1 + j)
-            plt.title('Original Image')
-            plt.imshow(squares[i*8+j], cmap='gray')
-
-            plt.subplot(4, 8, 9 + j)
-            plt.title('Enhanced Contrast')
-            plt.imshow(contrast_squares[i*8+j], cmap='gray')
-
-            plt.subplot(4, 8, 17 + j)
-            plt.title('Histogram (Original)')
-            plt.hist(squares[i*8+j].ravel(), bins=256, range=[0, 256], color='black')
-
-            plt.subplot(4, 8, 25 + j)
-            plt.title('Histogram (Enhanced)')
-            plt.hist(contrast_squares[i*8+j].ravel(), bins=256, range=[0, 256], color='black')
-
-            # plt.subplot(4, 8, 17 + j)
-            # plt.title('Thresholded')
-            # plt.imshow(preprocessed_squares[i*8+j], cmap='gray')
-
-            # plt.subplot(4, 8, 25 + j)
-            # plt.title('Has piece: ' + str(has_pieces[j][0]))
-            # plt.imshow(has_pieces[i*8+j][1], cmap='gray')
-    
-        plt.show()
-
-def chessboard_detection_3_hough(img):
-    """
-    Directly find chessboard corners in img2 and warps it to square and crops it.
-    """
     corners = bdh.find_board_corners(img)
     # find offsets to overlay img 1 and 2 perfectly
     cropped_image = project_board_2_square(img, corners) 
-    
-    board_colors = board_background_labels()
-    # TODO: Pick up coding here 
 
     # split the board int
     squares = split_image_into_squares(cropped_image)
@@ -260,119 +103,4 @@ def chessboard_detection_3_hough(img):
     # shrink the squares to remove the border
     squares = crop_squared_by_border_width(squares, border_width=1)
 
-    # enhance the contrast of the squares
-    gray_squares = [cv2.cvtColor(square, cv2.COLOR_BGR2GRAY) for square in squares]
-    contrast = [cv2.equalizeHist(square) for square in gray_squares]
-
-    # preprocess the squares
-    preprocessed_squares = [preprocess_square(square) for square in contrast]
-
-    # check if the square has a piece
-    has_pieces = [has_piece(square) for square in preprocessed_squares]
-
-    # Display the results
-    # display_squares(squares, contrast, preprocessed_squares, has_pieces)
-
-    return [-1] * 64
-
-def convert_ids_to_words(labels):
-    """
-    Coverts IDS 1, 0, -1 to correspnding colors white, none and black.
-
-    Parameters:
-    labels: array[64] of ids.
-    Returns:
-    array[64] of strings.
-    """
-
-    mapping = ["black", "none", "white"]
-    labels = [mapping[id+1] for id in labels]
-    
-    return labels
-
-def board_background_labels():
-    """
-    Returns the labels for the background of the chessboard.
-
-    returns:
-    array[64] of strings.
-    """
-
-    labels = []
-    for i in range(8):
-        for j in range(8):
-            if (i+j) % 2 == 0:
-                labels.append("black")
-            else:
-                labels.append("white")
-                
-    return labels
-    
-
-def label_chessboard(img):
-    """
-    Labels the chessboard squares with detected pieces and their background colors.
-    This function takes an image of a chessboard, detects the pieces on each square,
-    and assigns a label to each square indicating the piece and the background color.
-    Args:
-        img (numpy.ndarray): The input image of the chessboard.
-    Returns:
-        dict: A dictionary where each key is a square identifier (e.g., "A1", "B1", etc.)
-              and the value is another dictionary with keys "piece" and "background".
-              The "piece" key maps to the detected piece on that square (as a string),
-              and the "background" key maps to the background color of the square.
-    Example:
-        >>> img = cv2.imread('chessboard.jpg')
-        >>> labels = label_chessboard(img)
-        >>> print(labels["A1"])
-        {'piece': 'white', 'background': 'white'}
-    """
-    label_ids = chessboard_detection_3_hough(img)
-    labels_words = convert_ids_to_words(label_ids)
-
-    square_colour = board_background_labels()
-
-    SQUARES = [
-        "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1",
-        "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2",
-        "a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3",
-        "a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4",
-        "a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5",
-        "a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6",
-        "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7",
-        "a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8",
-    ]
-
-    label_dict = {}
-
-    for i in range(64):
-        label_dict[SQUARES[i]] = {"piece": labels_words[i], "background": square_colour[i]}
-
-    return label_dict
-
-def label_dict_to_str(label_dict):
-    """
-    Inputs a dict label_dict[SQUARES[i]] = {"piece": labels_words[i], "background": square_colour[i]}
-
-    outputs a dict label_dict_words[SQUARES[i]] = f"{labels_words[i]} on {square_colour[i]}"
-    """
-
-    label_dict_words = {}
-
-    for square, labels in label_dict.items():
-        label_dict_words[square] = f"{labels['piece']} on {labels['background']}"
-
-    return label_dict_words
-
-def main():
-    img1 = str(Path("Chessboard_detection", "TestImages", "Temp", "1.jpg"))
-    blank_board = cv2.imread(img1)
-
-    img_path2 = str(Path("Chessboard_detection", "TestImages", "Temp", "a2.jpg"))
-    img2 = cv2.imread(img_path2)
-
-    # find_warped_2(img1, img2)
-    chessboard_detection_3_hough(blank_board, img2)
-
-if __name__ == "__main__":
-    main()
+    return squares
