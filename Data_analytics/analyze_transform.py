a777b40a1bfa1c16fc5bda3af2e47cebbc2d5af4
Tiaan <spies.tiaan@gmail.com>
2025-02-25 15:01:29 -0500

    Added plotting functions to visualize data

--- Data_analytics/analyze_transform.py
+++ Data_analytics/analyze_transform.py
@@ -26,6 +26,9 @@ def main():
         "6: Analyze transformed results\n"
         )
     
+    plots_for_report()
+    return
+
     if reply == '1':
         recorded_with_transformation()
     elif reply == '2':
@@ -40,7 +43,135 @@ def main():
         analyze_transformed_results()
     else:
         print("Invalid input")
-        sys.exit()       
+        sys.exit()    
+
+
+def plots_for_report(): 
+
+    # ==================== Recorded with transformation ====================
+    message = "Select measured file: "
+    date, suffix, ext = get_matching_file_name(dirs.PATH_WIN_CAL_TRACKING_DATA, message, "*_measured*")
+
+    name_real = date+"_measured"+suffix+ext
+    name_planned = date+"_planned_path"+suffix+ext
+
+    # Load the numpy files for current and actual positions
+    pts_real = np.load(Path(dirs.PATH_WIN_CAL_TRACKING_DATA, name_real))
+    pts_planned = np.load(Path(dirs.PATH_WIN_CAL_TRACKING_DATA, name_planned))
+
+    message="\nSelect ideal file: "
+    prefix, suffix, ext= get_matching_file_name(dirs.PATH_WIN_PLANNED_PATHS, message, "*_path_ideal*")
+
+    file_ideal = Path(dirs.PATH_WIN_PLANNED_PATHS, prefix+"_path_ideal"+suffix+ext)
+    pts_ideal = np.load(file_ideal)
+
+    # remove NAN points
+    mask = ~(np.isnan(pts_real).any(axis=0) | np.isnan(pts_ideal).any(axis=0))
+    pts_ideal_w = pts_ideal[:, mask]
+    pts_real_w = pts_real[:, mask]
+    pts_planned = pts_planned[:, mask]
+
+    # ==================== Recorded without transformation ====================
+    message="Select measured file: "
+    date, suffix, ext = get_matching_file_name(dirs.PATH_WIN_CAL_TRACKING_DATA, message, identifier="*_measured*")
+
+    name_real = date+"_measured"+suffix+ext
+    file_real = Path(dirs.PATH_WIN_CAL_TRACKING_DATA, name_real) 
+
+    # Load the numpy files for current and actual positions
+    pts_real = np.load(file_real)
+
+    file_ideal = Path(dirs.PATH_WIN_CAL_TRACKING_DATA, date+"_planned_path"+suffix+ext)
+    pts_ideal = np.load(file_ideal)
+
+    #remove points that contain NAN
+    mask = ~(np.isnan(pts_real).any(axis=0) | np.isnan(pts_ideal).any(axis=0))
+    pts_ideal_wo = pts_ideal[:, mask]
+    pts_real_wo = pts_real[:, mask]
+
+    H = correction_transform.attempt_minimize_quad(pts_ideal_wo, pts_real_wo)
+
+    print(f"Transformation matrix:\n{H}")
+    pts_projected = correction_transform.project_points_quad(pts_real_wo, H)
+
+    plot_comparison_euclid_error_hist(pts_ideal_w, pts_real_w, pts_ideal_wo, pts_projected)
+    plot_comparison_individual_error_hist(pts_ideal_w, pts_real_w, pts_ideal_wo, pts_projected)
+
+    fig = create_plot_canvas(name_real)
+    plot_3data(pts_real, fig, "Measured Points")
+    plot_3data(pts_ideal, fig, "Nominal Points")
+    plot_3data(pts_projected, fig, "Projected after compensation")
+    
+    # Show the plot
+    fig.show()
+    # ==================== Visualise lin regression ====================
+    # Create a line plot
+    fig = go.Figure()
+
+    ys = np.linspace(0, 500, 200)
+
+    x = 106.25
+    z = 130
+    # [x'] = [1  2  3  4  5  7  8  9  10 11][x^2 -----------------]
+    # [y'] = [12 13 14 15 16 17 18 19 20 21][y^2 -----------------]
+    # [z'] = [22 23 24 25 26 27 28 29 30 31][z^2 -----------------]
+    #                                         [xy ------------------]
+    #                                         [xz ------------------]
+    #                                         [yz ------------------]
+    #                                         [x -------------------]
+    #                                         [y -------------------]
+    #                                         [z -------------------]
+    #                                         [1 -------------------]
+    zs_comp = x*x*H[2,0] + ys*ys*H[2, 1] + z*z*H[2, 2] + x*ys*H[2, 3] + x*z*H[2, 4] + ys*z*H[2, 5] + \
+         x*H[2, 6] + ys*H[2, 7] + z*H[2, 8] + H[2, 9]
+
+    fig.add_trace(go.Scatter(
+        x=ys,
+        y=[z]*len(ys),
+        mode='lines',
+        name='Nominal Z value'
+    ))
+
+    fig.add_trace(go.Scatter(
+        x=ys,
+        y=zs_comp,
+        mode='lines',
+        name='Compensated Z values'
+    ))
+
+    mask_x_106_z_159_wo = (pts_ideal_wo[0, :] == x) & (pts_ideal_wo[2, :] == z)
+    pts_real_wo_106_159 = pts_real_wo[2, mask_x_106_z_159_wo]
+
+    print(f"Count of matches in mask: {np.sum(mask_x_106_z_159_wo)}")
+
+    
+    mask_x_106_z_159_w = (pts_ideal_w[0, :] == x) & (pts_ideal_wo[2, :] == z)
+    pts_real_w_106_159 = pts_real_w[2, mask_x_106_z_159_w]
+    ys = np.linspace(420, 130, len(pts_real_wo_106_159))
+    fig.add_trace(go.Scatter(
+        x=ys,
+        y=pts_real_wo_106_159,
+        mode='markers',
+        name='Measured Z values without compensation'
+    ))
+
+    fig.add_trace(go.Scatter(
+        x=ys,
+        y=pts_real_w_106_159,
+        mode='markers',
+        name='Measured Z values with compensation'
+    ))
+
+    # Set y-axis range
+    fig.update_yaxes(range=[0, 200])
+    fig.update_xaxes(title_text='Y position (mm)')
+    fig.update_yaxes(title_text='Z position (mm)')
+    fig.update_layout(title='Snippet of Compensation on Z values')
+
+    # Show the plot
+    fig.show()
+
+
 
 def create_plot_canvas(a='3D Scatter Plot'):
     fig = go.Figure()
@@ -96,9 +227,9 @@ def recorded_with_transformation():
     pts_planned = pts_planned[:, mask]
 
     fig = create_plot_canvas(name_real)
-    plot_3data(pts_real, fig, "Pts_real")
-    plot_3data(pts_ideal, fig, "pts_ideal")
-    plot_3data(pts_planned, fig, "Planned")
+    plot_3data(pts_real, fig, "Measured Points")
+    plot_3data(pts_ideal, fig, "Nominal Points")
+    plot_3data(pts_planned, fig, "Compensated Points")
 
     # Show the plot
     fig.show()
@@ -230,12 +361,13 @@ def recorded_without_transformation():
     print(f"Max Error between ideal and projected: {find_max_error(pts_ideal, pts_projected)}")
     print(f"Mean Error between ideal and projected: {find_mean_error(pts_ideal, pts_projected)}")
 
-    plot_euclid_error_hist(pts_ideal, pts_projected)
+    plot_euclid_error_hist(pts_ideal, pts_real)
+    plot_individual_error_hist(pts_ideal, pts_real)
 
     fig = create_plot_canvas(name_real)
-    plot_3data(pts_real, fig, "Pts_real")
-    plot_3data(pts_ideal, fig, "pts_ideal")
-    plot_3data(pts_projected, fig, "Projected")
+    plot_3data(pts_real, fig, "Measured Points")
+    plot_3data(pts_ideal, fig, "Nominal Points")
+    plot_3data(pts_projected, fig, "Projected after compensation")
     
     # Show the plot
     fig.show()
@@ -342,33 +474,121 @@ def find_mean_error(pts_truth, pts_test):
 
     return mean_error
 
+def plot_comparison_individual_error_hist(pts_real_w, pts_test_with_transformation, pts_real_wo, pts_test_without_transformation):
+    """Plot histograms comparing the individual axis errors between the truth and test points with and without transformation"""
+
+    # Calculate the error for both with and without transformation
+    error_with_transformation = pts_real_w - pts_test_with_transformation
+    error_without_transformation = pts_real_wo - pts_test_without_transformation
+
+    # Create a histogram
+    fig = make_subplots(rows=3, cols=1)
+    fig.update_layout(title_text='Histograms of Individual Axis Error Distributions Comparison')
+
+    axis_labels = ["X position error (mm)", "Y position error (mm)", "Z position error (mm)"]
+    colors = ['blue', 'red']
+
+    for i in range(3):
+        fig.add_trace(go.Histogram(
+            x=error_without_transformation[i, :],
+            xbins=dict(size=1),
+            opacity=0.5,
+            marker_color=colors[1],
+            showlegend=(i == 0),
+            name='Predicted Error After Calibration'
+        ), row=i+1, col=1)
+
+        fig.add_trace(go.Histogram(
+            x=error_with_transformation[i, :],
+            xbins=dict(size=1),
+            opacity=0.5,
+            marker_color=colors[0],
+            showlegend=(i == 0),
+            name='Measured Error After Calibration'
+        ), row=i+1, col=1)
+
+        fig.update_yaxes(title_text="Count", row=i+1, col=1)
+        fig.update_xaxes(title_text=axis_labels[i], range=[-25, 30], dtick=5, row=i+1, col=1)
+
+    fig.update_layout(barmode='overlay')
+    fig.show()
+
+def plot_comparison_euclid_error_hist(pts_truth_w, pts_test_with_transformation, pts_truth_wo, pts_test_without_transformation):
+    """Plot a histogram comparing the error between the truth and test points with and without transformation"""
+
+    # Calculate the error for both with and without transformation
+    error_with_transformation = np.sqrt(np.sum((pts_truth_w - pts_test_with_transformation)**2, axis=0))
+    error_without_transformation = np.sqrt(np.sum((pts_truth_wo - pts_test_without_transformation)**2, axis=0))
+
+    # Filter errors to be less than 40
+    error_with_transformation = error_with_transformation[error_with_transformation < 40]
+    error_without_transformation = error_without_transformation[error_without_transformation < 40]
+
+    # Create a histogram
+    fig = go.Figure()
+
+    fig.add_trace(go.Histogram(
+        x=error_without_transformation,
+        xbins=dict(size=1),
+        opacity=0.5,
+        name='Predicted Error After Calibration',
+        marker_color='red'
+    ))
+
+    fig.add_trace(go.Histogram(
+        x=error_with_transformation,
+        xbins=dict(size=1),
+        opacity=0.5,
+        name='Measured Error After Calibration',
+        marker_color='blue'
+    ))
+
+    fig.update_xaxes(title_text='Euclidian Position Error (mm)', range=[0, 30])
+    fig.update_yaxes(title_text='Count')
+    fig.update_layout(title_text='Histogram of Error Distribution Comparison')
+    fig.update_layout(
+        xaxis=dict(
+            dtick=5
+        ),
+        barmode='overlay'
+    )
+    fig.show()
+
 def plot_euclid_error_hist(pts_truth, pts_test):
     """Plot a histogram of the error between the truth and test points"""
 
     # Calculate the error
     error = np.sqrt(np.sum((pts_truth-pts_test)**2, axis=0))
-    error = error[error<40]
+    error = error[error < 40]
     # Create a histogram
-    # Create a histogram
-    fig = go.Figure(data=[go.Histogram(x=error, nbinsx=40)])
-    fig.update_xaxes(title_text='Position Error (mm)')
+
+    fig = go.Figure(data=[go.Histogram(x=error, xbins=dict(size=1))])
+    fig.update_xaxes(title_text='Abs Euclidian Position Error (mm)', range=[0, 30])
+    fig.update_yaxes(title_text='Count')
+    fig.update_layout(title_text='Histogram of Error Distribution Post Calibration')
     fig.update_layout(
         xaxis=dict(
-            dtick=0.5
+            dtick=5
         )
     )
     fig.show()
 
 def plot_individual_error_hist(pts_truth, pts_test):
-    """Plot a histogram for the difference between point and target for each axis seperate"""
+    """Plot a histogram for the difference between point and target for each axis separate"""
 
     # Calculate the error
     error = pts_truth - pts_test
 
     # Create a histogram
-    fig = make_subplots(rows=3, cols=1, subplot_titles=("X position error (mm)", "Y position error (mm)", "Z position error (mm)"))
+    fig = make_subplots(rows=3, cols=1)
+    fig.update_layout(title_text='Histograms of Individual Axis Error Distributions')
+
+    axis_labels = ["X position error (mm)", "Y position error (mm)", "Z position error (mm)"]
+
     for i in range(3):
-        fig.add_trace(go.Histogram(x=error[i, :]), row=i+1, col=1)
+        fig.add_trace(go.Histogram(x=error[i, :], xbins=dict(size=1)), row=i+1, col=1)
+        fig.update_yaxes(title_text="Count", row=i+1, col=1)
+        fig.update_xaxes(title_text=axis_labels[i], range=[-25, 30], dtick=5, row=i+1, col=1)
     fig.show()
 
 def get_matching_file_name(search_path:Path, message:str="Select a file: ", identifier:str="*_path_*"):
